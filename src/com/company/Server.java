99d3ed2ac91777902e482182687d104a6df9ea21
AnzePeharc <anze.peharc@gmail.com>
2020-12-22 12:45:43 +0100

    Android Studio socket testing environment

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -3,14 +3,26 @@ package com.company;
 import java.io.*;
 import java.net.*;
 public class Server {
+
+    static ServerSocket ss;
+    static Socket s;
+    static InputStreamReader isr;
+    static BufferedReader br;
+    static String message;
+
     public static void main(String[] args){
         try{
-            ServerSocket ss=new ServerSocket(8080);
-            Socket s=ss.accept();//establishes connection
-            DataInputStream dis=new DataInputStream(s.getInputStream());
-            String  str=(String)dis.readUTF();
-            System.out.println("message= "+str);
-            ss.close();
+            ss=new ServerSocket(8080);
+            while (true){
+                s=ss.accept();//establishes connection
+                isr = new InputStreamReader(s.getInputStream());
+                br = new BufferedReader(isr);
+                message= br.readLine();
+
+                System.out.println(message);
+            }
+
+
         }catch(Exception e){System.out.println(e);}
     }
 }
